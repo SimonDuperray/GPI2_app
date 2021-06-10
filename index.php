499a69e7fc21859c19948658fe5953d6615a1692
Kartodix <64204457+Kartodix@users.noreply.github.com>
2021-06-10 11:23:14 +0200

    index.php done

--- index.php
+++ index.php
@@ -4,22 +4,38 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-   <title>Projet</title>
+   <title>Connection-BaA</title>
+   <style>
+      #form-container {
+         background-color: lightblue;
+         border-radius: 10px;
+         padding: 20px;
+         position: absolute;
+         left: 50%;
+         top: 50%;
+         transform: translate(-50%, -50%);
+         width: 400px;
+         border: 1px solid black;
+         box-shadow: 4px 4px 4px grey;
+      }
+   </style>
 </head>
 <body>
-   <div class="container">
-      <h1>Form - Be An Artist</h1>
-      <form action="retourIndex.php" method="POST">
-         <div class="mb-3">
-            <label for="login" class="form-label">Login</label>
-            <input type="text" class="form-control" id="login" name="login">
-         </div>
-         <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password" name="password">
-         </div>
-         <input type="submit" class="btn btn-primary" name="submit" />
-      </form>
+   <div class="container" id="form-container">
+      <h1>Be An Artist</h1>
+      <div>
+         <form action="retourIndex.php" method="POST">
+            <div class="mb-3">
+               <label for="login" class="form-label">Login</label>
+               <input type="text" class="form-control" id="login" name="login" placeholder="Login">
+            </div>
+            <div class="mb-3">
+               <label for="password" class="form-label">Password</label>
+               <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+            </div>
+            <input type="submit" class="btn btn-primary" name="submit" value="Log In" />
+         </form>
+      </div>
    </div>
 </body>
 </html>
