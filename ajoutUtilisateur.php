f3b47934dfc384c3d762a1d9317760072266b838
Kartodix <64204457+Kartodix@users.noreply.github.com>
2021-06-10 13:04:30 +0200

    retourAjoutUtilisateur.php done

--- ajoutUtilisateur.php
+++ ajoutUtilisateur.php
@@ -1,33 +1,75 @@
-<?php
-$ADMINID = 10;
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">  
-        <title> Menu Administrateur </title>
+        <title>Admin Panel</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-        <script type="text/javascript" src="verificationFormulaire.js"></script>
+        <style>
+            .container-div {
+                border: 1px solid black;
+                position: absolute;
+                left: 50%;
+                top: 50%;
+                transform: translate(-50%, -50%);
+                padding: 50px;
+                background-color: lightblue;
+                width: 600px;
+                border-radius: 10px;
+            }
+            #form-container {
+                background-color: lightblue;
+                border-radius: 10px;
+                padding: 20px;
+                position: absolute;
+                left: 50%;
+                top: 50%;
+                transform: translate(-50%, -50%);
+                width: 400px;
+                border: 1px solid black;
+                box-shadow: 4px 4px 4px grey;
+            }
+            .link {
+                text-decoration: none;
+                color: #fff;
+            }
+            .link:hover {
+                color: black;
+                text-decoration: none;
+            }
+            #welcome {
+                text-align: center;
+            }
+            .btn-container {
+                text-align: center;
+            }
+            #msg-err {
+                text-align: center;
+                font-weight: bold;
+                font-size: 17px;
+                text-decoration: underline;
+            }
+            .pd {
+                margin-top: 5px;
+            }
+        </style>
     </head>
-
-
     <body>
-        
         <?php 
+            $ADMINID = 10;
 
-            // Récupération du numéro de l'utilisateur
             session_start();
             $numeroUtilisateur = $_SESSION['utilisateur'];
 
-            // Si Administrateur ... 
             if ($numeroUtilisateur == $ADMINID) {
         ?>
-            <div class="container">
+            <div class="container" id='form-container'>
                 <form action="retourAjoutUtilisateur.php" method="POST">
+                    <h3 id='welcome'>Add User</h3>
                     <div class="mb-3">
                         <label for="name" class="form-label">Name</label>
                         <input type="text" class="form-control" id="name" name="name">
+                    </div>
+                    <div class="mb-3">
                         <label for="firstname" class="form-label">Firstname</label>
                         <input type="text" class="form-control" id="firstname" name="firstname">
                     </div>
@@ -38,29 +80,27 @@ $ADMINID = 10;
                     <div class="mb-3">
                         <label for="login" class="form-label">Login</label>
                         <input type="text" class="form-control" id="login" name="login">
+                    </div>
+                    <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
                         <input type="password" class="form-control" id="password" name="password">
                     </div>
-                    <input type="submit" class="btn btn-primary" name="submit" />
+                    <input type="submit" class="btn btn-primary" name="submit" value="Add User" />
                 </form>
-                <button>
-                    <a href="menuAdministrateur.php">
+                <button class='btn btn-primary pd'>
+                    <a class='link' href="menuAdministrateur.php">
                         Back to menu
                     </a>
                 </button>
             </div>
-                
         <?php
-            }
-            else {
+            } else {
                 echo "
                 <p> Vous n'êtes pas Administrateur </p> <br>
                 <a href=\"index.php\"> Page de connexion </a>
                 ";
             }
-
-
         ?>
-
+        <script type="text/javascript" src="verificationFormulaire.js"></script>
     </body>
 </html>
--- retourAjoutUtilisateur.php
+++ retourAjoutUtilisateur.php
@@ -3,10 +3,42 @@
     <head>
         <meta charset="utf-8">   
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-        <title>Admin Panel</title>
+        <title>AddUserRes</title>
+        <style>
+            .container-div {
+                border: 1px solid black;
+                position: absolute;
+                left: 50%;
+                top: 50%;
+                transform: translate(-50%, -50%);
+                padding: 50px;
+                background-color: lightblue;
+                width: 600px;
+                border-radius: 10px;
+            }
+            .link {
+                text-decoration: none;
+                color: #fff;
+            }
+            .link:hover {
+                color: black;
+                text-decoration: none;
+            }
+            #welcome {
+                text-align: center;
+            }
+            .btn-container {
+                text-align: center;
+            }
+            #msg-err {
+                text-align: center;
+                font-weight: bold;
+                font-size: 17px;
+                text-decoration: underline;
+            }
+        </style>
     </head>
     <body>
-        
         <?php 
             $ADMINID = 10;
 
@@ -37,19 +69,36 @@
                     $request = "INSERT INTO Utilisateur VALUES (null,'".$name."','".$firstname."', '".$address."','".$login."', PASSWORD('".$password."'));";
 
                     $result = $mysqli->query($request);
+
+                    echo "
+                        <div class='container container-div'>
+                    ";
+
                     if(!$result) {
-                        echo "<h1 color='red'>Request failure</h1>";
+                        echo "<h1 id='msg-err' color='red'>Request failure</h1>";
                     } else {
                         echo "
-                            <a href=\"menuAdministrateur.php\"> Admin panel </a> <br><br>
+                            <h2 id='welcome'>User added !</h2>
+                            <div class='btn-container'>
+                                <button class='btn btn-primary'>
+                                    <a class='link' href=\"menuAdministrateur.php\">Admin panel</a>
+                                </button>
+                            </div>
                         ";
                     }
+                    echo "</div>";
                     $mysqli->close();
                 }
             } else {
                 echo "
-                    <p>You're not admin</p><br>
-                    <a href='index.php'>Log In</a>
+                    <div class='container container-div'>
+                        <p id='msg-err'>You're not admin</p>
+                        <div class='btn-container'>
+                            <button class='btn btn-primary'>
+                                <a class='link' href=\"index.php\">Log In</a>
+                            </button>
+                        </div>
+                    </div>
                 ";
             }
         ?>
