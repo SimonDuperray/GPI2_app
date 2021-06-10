96b73040c6b796fd11b11b548bdc75cbbf9fb5f9
Kartodix <64204457+Kartodix@users.noreply.github.com>
2021-06-10 12:22:17 +0200

    adminpanel.php done

--- listeConcours.php
+++ listeConcours.php
@@ -1,41 +1,66 @@
-<?php
-$ADMINID = 10;
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">  
-        <title> Menu Administrateur </title>
+        <title>Admin panel</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-        <script type="text/javascript" src="verificationFormulaire.js"></script>
+        <style>
+            .container-div {
+                border: 1px solid black;
+                position: absolute;
+                left: 50%;
+                top: 50%;
+                transform: translate(-50%, -50%);
+                padding: 50px;
+                background-color: lightblue;
+                width: 600px;
+                border-radius: 10px;
+            }
+            .link {
+                text-decoration: none;
+                color: #fff;
+            }
+            .link:hover {
+                color: black;
+                text-decoration: none;
+            }
+            #welcome {
+                text-align: center;
+            }
+            .btn-container {
+                text-align: center;
+            }
+            #msg-err {
+                text-align: center;
+                font-weight: bold;
+                font-size: 17px;
+                text-decoration: underline;
+            }
+        </style>
     </head>
-
-
     <body>
-        
         <?php 
+            $ADMINID = 10;
 
-            // Récupération du numéro de l'utilisateur
             session_start();
             $numeroUtilisateur = $_SESSION['utilisateur'];
 
-            // Si Administrateur ... 
             if ($numeroUtilisateur == $ADMINID) {
-
                 $mysqli = new mysqli("localhost", "gpi2", "network", "projet");
                 if($mysqli->connect_error) {
-                    exit('Impossible de se connecter à la BD');
+                    exit('DDB connection error');
                 }
                 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
                 $mysqli->set_charset("utf8");
-
 
                 $maRequete = "SELECT * FROM Concours WHERE 1";
 
                 $result = $mysqli->query($maRequete); 
 
-                echo "<ul>";
+                echo "
+                    <div class='container container-div'>
+                        <ul>
+                ";
                 while($row = mysqli_fetch_assoc($result)){
                     echo "<li>".$row['numConcours']."-".$row['description']."</li>";
                 }
@@ -43,18 +68,22 @@ $ADMINID = 10;
     
                 $mysqli->close();
                 
-                echo "<a href=\"menuAdministrateur.php\"> Back </a> <br>";
+                echo "<a href=\"menuAdministrateur.php\"> Back </a>";
+                echo "</div>";
         
-            }
-            else {
+            } else {
                 echo "
-                <p> Vous n'êtes pas Administrateur </p> <br>
-                <a href=\"index.php\"> Page de connexion </a>
+                    <div class='container container-div'>
+                        <p id='msg-err'>You're not admin</p>
+                        <div class='btn-container'>
+                            <button class='btn btn-primary'>
+                                <a class='link' href=\"index.php\">Log In</a>
+                            </button>
+                        </div>
+                    </div>
                 ";
             }
-
-
         ?>
-
+        <script type="text/javascript" src="verificationFormulaire.js"></script>
     </body>
 </html>
--- menuAdministrateur.php
+++ menuAdministrateur.php
@@ -1,40 +1,77 @@
-<?php
-$ADMINID = 10;
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">  
-        <title> Menu Administrateur </title>
+        <meta charset="utf-8"> 
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"> 
+        <title>Admin panel</title>
+        <style>
+            .container-div {
+                border: 1px solid black;
+                position: absolute;
+                left: 50%;
+                top: 50%;
+                transform: translate(-50%, -50%);
+                padding: 50px;
+                background-color: lightblue;
+                width: 600px;
+                border-radius: 10px;
+            }
+            .link {
+                text-decoration: none;
+                color: #fff;
+            }
+            .link:hover {
+                color: black;
+                text-decoration: none;
+            }
+            #welcome {
+                text-align: center;
+            }
+            .btn-container {
+                text-align: center;
+            }
+            #msg-err {
+                text-align: center;
+                font-weight: bold;
+                font-size: 17px;
+                text-decoration: underline;
+            }
+        </style>
     </head>
-
-
     <body>
-        
-        <?php 
+        <?php
+        $ADMINID = 10; 
 
-            // Récupération du numéro de l'utilisateur
-            session_start();
-            $numeroUtilisateur = $_SESSION['utilisateur'];
+        session_start();
+        $numeroUtilisateur = $_SESSION['utilisateur'];
 
-            // Si Administrateur ... 
-            if ($numeroUtilisateur == $ADMINID) {
-                echo "
-                <button> <a href=\"ajoutUtilisateur.php\"> Ajouter un utilisateur </a> <br> </button>
-                <button> <a href=\"creationConcours.php\"> Créer un concours </a> <br> </button>
-                ";
-            }
-            else {
-                echo "
-                <p> Vous n'êtes pas Administrateur </p> <br>
-                <a href=\"index.php\"> Page de connexion </a>
-                ";
-            }
-
-
+        if ($numeroUtilisateur == $ADMINID) {
+            echo "
+            <div class='container container-div'>
+                <h2 id='welcome'>Admin panel</h2>
+                <div class='btn-container'>
+                    <button class='btn btn-primary'>
+                        <a class='link' href=\"ajoutUtilisateur.php\">Add user</a>
+                    </button>
+                    <button class='btn btn-primary'>
+                        <a class='link' href=\"creationConcours.php\">Create competition</a>
+                    </button>
+                </div>
+            </div>
+            ";
+        } else {
+            echo "
+                <div class='container container-div'>
+                    <p id='msg-err'>You're not admin</p>
+                    <div class='btn-container'>
+                        <button class='btn btn-primary'>
+                            <a class='link' href=\"index.php\">Log In</a>
+                        </button>
+                    </div>
+                </div>
+            ";
+        }
         ?>
-
     </body>
 </html>
 
