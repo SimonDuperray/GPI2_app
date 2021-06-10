e42f7bbe4dd5677e10de9dc8c72c18ca031dd1b3
Kartodix <64204457+Kartodix@users.noreply.github.com>
2021-06-10 12:03:10 +0200

    remove lines

--- retourAjoutUtilisateur.php
+++ retourAjoutUtilisateur.php
@@ -1,86 +1,57 @@
-<?php
-$ADMINID = 10;
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">  
-        <title> Menu Administrateur </title>
+        <meta charset="utf-8">   
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        <title>Admin Panel</title>
     </head>
-
-
     <body>
         
         <?php 
+            $ADMINID = 10;
 
-            // Récupération du numéro de l'utilisateur
             session_start();
             $numeroUtilisateur = $_SESSION['utilisateur'];
 
-            // Si Administrateur ... 
             if ($numeroUtilisateur == $ADMINID) {
+                if (isset($_POST['name']) and isset($_POST['firstname']) ) {
+                    $name = htmlspecialchars($_POST['name']);
+                    $firstname = htmlspecialchars($_POST['firstname']);
+                    $address = htmlspecialchars($_POST['address']);
+                    $login = htmlspecialchars($_POST['login']);
+                    $password = htmlspecialchars($_POST['password']);
 
-
-
-
-                // Récupération des réponses
-                if (isset($_POST['name']) and isset($_POST['firstname']) ) {                    // vérifie que le champ du formulaire est bien rempli
-                    $name = htmlspecialchars($_POST['name']);                                 // stocke les réponses, en convertissant les possibles caractères particuliers
-                    $firstname = htmlspecialchars($_POST['firstname']);                               
-                    $address = htmlspecialchars($_POST['address']);  
-                    $login = htmlspecialchars($_POST['login']);  
-                    $password = htmlspecialchars($_POST['password']);  
-
-                    
-
-
-
-                    // Connexion à la base données 
                     $mysqli = new mysqli("localhost", "gpi2", "network", "projet");
                     if($mysqli->connect_error) {
-                        exit('Impossible de se connecter à la BD');
+                        exit('DDB connection error');
                     }
                     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
                     $mysqli->set_charset("utf8");
                     
-
-                    // associe variables ??
                     $name = $mysqli->real_escape_string($name);
                     $firstname = $mysqli->real_escape_string($firstname);
                     $address = $mysqli->real_escape_string($address);
                     $login = $mysqli->real_escape_string($login);
                     $password = $mysqli->real_escape_string($password);
 
-                    $maRequete = "INSERT INTO Utilisateur VALUES (null,'".$name."','".$firstname."', '".$address."','".$login."', PASSWORD('".$password."'));";   // Requête SQL
+                    $request = "INSERT INTO Utilisateur VALUES (null,'".$name."','".$firstname."', '".$address."','".$login."', PASSWORD('".$password."'));";
 
-
-                
-                    // Résultat de la requête
-                    $result = $mysqli->query($maRequete);                   // Si requête éxécutée
+                    $result = $mysqli->query($request);
                     if(!$result) {
-                        echo "La requête ne s’est pas exécutée ";
-                    } 
-                    else {
+                        echo "<h1 color='red'>Request failure</h1>";
+                    } else {
                         echo "
-                        La requête s’est bien exécutée <br>
-                        <a href=\"menuAdministrateur.php\"> Page administrateur </a> <br><br>";
-                        // $result->free();
+                            <a href=\"menuAdministrateur.php\"> Admin panel </a> <br><br>
+                        ";
                     }
                     $mysqli->close();
                 }
-
-
-            }
-            else {
+            } else {
                 echo "
-                <p> Vous n'êtes pas Administrateur </p> <br>
-                <a href=\"index.php\"> Page de connexion </a>
+                    <p>You're not admin</p><br>
+                    <a href='index.php'>Log In</a>
                 ";
             }
-
-
         ?>
-
     </body>
 </html>
--- retourIndex.php
+++ retourIndex.php
@@ -1,31 +1,54 @@
-<?php
-$ADMINID = 10;
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">  
-        <title>Page de traitement</title>
+        <meta charset="utf-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        <title>Treatment page</title>
+        <style>
+            .container-div {
+                border: 1px solid black;
+                position: absolute;
+                left: 50%;
+                top: 50%;
+                transform: translate(-50%, -50%);
+                padding: 50px;
+                background-color: lightblue;
+                width: 600px;
+                border-radius: 10px;
+            }
+            .link {
+                text-decoration: none;
+                color: #fff;
+            }
+            .link:hover {
+                color: black;
+                text-decoration: none;
+            }
+            #welcome {
+                text-align: center;
+            }
+            .btn-container {
+                text-align: center;
+            }
+            #msg-err {
+                text-align: center;
+                font-weight: bold;
+                font-size: 17px;
+                text-decoration: underline;
+            }
+        </style>
     </head>
-
-
     <body>
-        <!-- <p>Dans le formulaire précédent, vous avez fourni les
-        informations suivantes :</p> -->
-        
-        <?php 
+        <?php
+        $ADMINID = 10;
 
-        // Récupération des réponses
         if (isset($_POST['login']) and isset($_POST['password']) ) {
             $login = htmlspecialchars($_POST['login']);
             $pwd = htmlspecialchars($_POST['password']);
         
-
-            // Connexion à la base données 
             $mysqli = new mysqli("localhost", "gpi2", "network", "projet");
             if($mysqli->connect_error) {
-                exit('Impossible de se connecter à la BD');
+                exit('DDB connection error');
             }
             mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
             $mysqli->set_charset("utf8");
@@ -34,53 +57,56 @@ $ADMINID = 10;
             $pwd = $mysqli->real_escape_string($pwd);
             $maRequete = "SELECT * FROM Utilisateur WHERE login = '".$login."' AND motDePasse = PASSWORD('".$pwd."');";
 
-            // Résultat de la requête
             $result = $mysqli->query($maRequete);                  
             if(!$result) {
-                echo "La requête ne s’est pas exécutée";
-            } 
-            else {
-                echo "La requête s’est bien exécutée";
+                echo "Request error";
+            } else {
                 $resultat = $result->fetch_assoc(); 
-
                 if ($resultat){
-                    echo "<p> Connexion réussie ! </p><br>";
-
-                    // mémoriser une valeur :
                     session_start();
                     $_SESSION['utilisateur'] = $resultat["numUtilisateur"];
                     $_SESSION['login'] = $login;
-                    
-                    // vérifie si l'utilisateur est Administrateur
-                    if($resultat["numUtilisateur"] == $ADMINID) {                                              
-                        echo "<a href=\"menuAdministrateur.php\"> Admin panel </a> <br> <br>";
-                    }
-                    echo "<a href=\"listeConcours.php\"> Liste des concours </a> <br>";
-                }       
-                else {
+
                     echo "
-                    <p> Failed connection </p><br>
-                    <a href=\"index.php\"> Page de connexion </a>
+                        <div class='container container-div'>
+                            <h3 id='welcome'>Welcome ".$resultat['prenom']." !</h3>
+                            <div class='btn-container'>
+                    ";
+                    
+                    if($resultat["numUtilisateur"] == $ADMINID) {                                              
+                        echo "
+                            <button class='btn btn-danger'>
+                                <a class='link' href=\"menuAdministrateur.php\">Admin panel</a>
+                            </button>
+                        ";
+                    }
+                    echo "
+                        <button class=' btn btn-primary'>
+                            <a class='link' href=\"listeConcours.php\">See Drawing Competition List</a>
+                        </button>
+                        </div></div>
+                    ";
+                } else {
+                    echo "
+                        <div class='container container-div'>
+                            <p id='msg-err'>Failed connection</p>
+                            <div class='btn-container'>
+                                <button class='btn btn-primary'>
+                                    <a class='link' href=\"index.php\">Log In</a>
+                                </button>
+                            </div>
+                        </div>
                     ";
                 }
                 $result->free();
             }
             $mysqli->close();
-            
-        
-        }
-        else { 
+        } else { 
             echo "
-            <p> Unknown user </p><br>
-            <a href=\"index.php\"> Page de connexion </a>
+                <p> Unknown user </p><br>
+                <a href=\"index.php\">Log In</a>
             ";
         }
-
         ?>
-
-
     </body>
 </html>
-
-
-
